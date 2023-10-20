5d4160138b8ad1bad45d373a29515534f9dc96b7
Gray <graychen1999@gmail.com>
2023-10-20 03:15:54 -0700

    cleaning the directories

--- pq_client.py
+++
@@ -1,8 +0,0 @@
-import oqs
-
-
-kemalg = "Kyber512"
-with oqs.KeyEncapsulation(kemalg) as client:
-    client_public_key = client.generate_keypair()
-    client_secret_key = client.export_secret_key()
-    
